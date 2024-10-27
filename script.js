d4d499b730245ec8b3cb4b68db13c2b1ec2763ed
Tengku Aiman <147373281+Tmanlip@users.noreply.github.com>
2024-10-27 15:13:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const body = document.querySelector("body"),
+    nav = document.querySelector("nav"),
+    modeToggle = document.querySelector(".dark-light"),
+    searchToggle = document.querySelector(".searchToggle"),
+    sideBarOpen = document.querySelector(".sideBarOpen"),
+    sideBarClosed = document.querySelector(".sideBarClosed");
+
+    let getMode = localStorage.getItem("mode") ;
+    if (getMode && getMode == "dark-mode"){
+        body.classList.add("dark");
+    }
+
+    //Js code to toggle dark and light mode
+    modeToggle.addEventListener("click", () =>{
+        modeToggle.classList.toggle("active");
+        body.classList.toggle("dark");
+
+        if(!body.classList.contains("dark")){
+            localStorage.setItem("mode", "light-mode");
+        }else{
+            localStorage.setItem("mode", "dark-mode");
+        }
+    })
+
+    //Js code to toggle search box
+    searchToggle.addEventListener("click", () =>{
+        searchToggle.classList.toggle("active");
+    })
+
+
+    //Js code to toggle sidebar
+    sideBarOpen.addEventListener("click", () =>{
+        nav.classList.add("active");
+    })
+
+    body.addEventListener("click", e =>{
+        let clickedElm = e.target;
+
+        if (!clickedElm.classList.contains("sideBarOpen") && !clickedElm.classList.contains("menu")){
+            nav.classList.remove("active");
+        }
+    })
+
