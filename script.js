db2a9fb6b93f468be7f0f907d5b1539ebbe119d6
Tengku Aiman <tengku03@graduate.utm.my>
2024-10-28 23:51:59 +0800

    p

--- script.js
+++ script.js
@@ -5,10 +5,20 @@ const body = document.querySelector("body"),
     sideBarOpen = document.querySelector(".sideBarOpen"),
     sideBarClosed = document.querySelector(".sideBarClosed");
 
+//Js Code for toggle user
+const dashToggle = document.querySelector(".home"),
+    userToggle = document.querySelector(".home-2"),
+    acadToggle = document.querySelector(".home-3"),
+    firstItems = document.querySelector(".first-items"),
+    secondItems = document.querySelector(".second-items"),
+    thirdItems = document.querySelector(".third-items");
+
     let getMode = localStorage.getItem("mode") ;
     if (getMode && getMode == "dark-mode"){
         body.classList.add("dark");
     }
+
+
 
     //Js code to toggle dark and light mode
     modeToggle.addEventListener("click", () =>{
@@ -40,4 +50,34 @@ const body = document.querySelector("body"),
             nav.classList.remove("active");
         }
     })
+
+    //Js Code to toggle visibility of content
+    dashToggle.addEventListener("click", ()=>{
+        firstItems.classList.toggle("displayed");
+        firstItems.classList.toggle("hidden");
+        secondItems.classList.add("hidden"); // Hide user items if dashboard is shown
+        secondItems.classList.remove("displayed");
+        thirdItems.classList.add("hidden"); // Hide user items if dashboard is shown
+        thirdItems.classList.remove("displayed");
+    })
+
+    userToggle.addEventListener("click", ()=>{
+        secondItems.classList.toggle("displayed");
+        secondItems.classList.toggle("hidden");
+        firstItems.classList.add("hidden"); // Hide dashboard items if user is shown
+        firstItems.classList.remove("displayed");
+        thirdItems.classList.add("hidden"); // Hide user items if dashboard is shown
+        thirdItems.classList.remove("displayed");
+    })
+
+    acadToggle.addEventListener("click", ()=>{
+        thirdItems.classList.toggle("displayed");
+        thirdItems.classList.toggle("hidden");
+        firstItems.classList.add("hidden"); // Hide dashboard items if user is shown
+        firstItems.classList.remove("displayed");
+        secondItems.classList.add("hidden"); // Hide user items if dashboard is shown
+        secondItems.classList.remove("displayed");
+    })
+
+    
 
